ec9f2254e257e6f2b4905fbfb29b451609522ad8
FelippeTN <felippenalim2004@gmail.com>
2025-11-04 23:11:31 -0300

    feat:  lifespan

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import uvicorn
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -7,9 +8,19 @@ from app.core.config import get_settings
 from app.core.database import init_db
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+
 def create_app() -> FastAPI:
     settings = get_settings()
-    application = FastAPI(title=settings.app_name, debug=settings.debug)
+    application = FastAPI(
+        title=settings.app_name, 
+        debug=settings.debug,
+        lifespan=lifespan
+    )
 
     application.add_middleware(
         CORSMiddleware,
@@ -23,10 +34,6 @@ def create_app() -> FastAPI:
     application.include_router(guests.router, prefix=settings.api_v1_prefix)
     application.include_router(rooms.router, prefix=settings.api_v1_prefix)
     application.include_router(bookings.router, prefix=settings.api_v1_prefix)
-
-    @application.on_event("startup") # refactor: Criar lifespam pois esse modo esta depreciado
-    def _startup() -> None:
-        init_db()
 
     @application.get("/")
     def read_root():
